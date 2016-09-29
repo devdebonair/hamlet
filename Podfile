24eda563c04fd50cacb5e08bf0469c5fc3044290
Vincent Moore <devdebonair@gmail.com>
2016-09-29 00:14:48 -0400

    added cocoapods and alamofire

--- Podfile
+++ Podfile
@@ -2,6 +2,6 @@ platform :ios, '10.0'
 use_frameworks!
 
 target 'Hamlet' do
-
+	pod 'Alamofire', '~> 4.0.1'
 end
 
---
+++ Pods/Target Support Files/Pods-Hamlet/Pods-Hamlet-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HamletVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HamletVersionString[];
+
