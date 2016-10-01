508a83c22e9340f6fe7b0da98a833dabad539764
Vincent Moore <devdebonair@gmail.com>
2016-10-01 13:14:24 -0400

    added identifier extension for table view cell

---
+++ Hamlet/UITableViewCell+Identifier.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+Identifier.swift
+//  Hamlet
+//
+//  Created by Vincent Moore on 10/1/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    class var IDENTIFIER: String {
+        return "TableCell"
+    }
+}
