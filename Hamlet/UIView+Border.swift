4039be6c803e9cd3503454e8765cd1949c80173c
Vincent Moore <devdebonair@gmail.com>
2016-10-02 12:52:47 -0400

    added uiview extension to add borders

---
+++ Hamlet/UIView+Border.swift
@@ -0,0 +1,91 @@
+//
+//  UIView+Border.swift
+//  Instagram
+//
+//  Created by Vincent Moore on 5/29/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    // Helper function to add borders to any or multiple sides of a UIView
+    func addBorder(edges: UIRectEdge, colour: UIColor = UIColor.white, thickness: CGFloat = 1) {
+        
+        var borders = [UIView]()
+        
+        func border() -> UIView {
+            let border = UIView(frame: CGRect.zero)
+            border.backgroundColor = colour
+            border.translatesAutoresizingMaskIntoConstraints = false
+            return border
+        }
+        
+        if edges.contains(.top) || edges.contains(.all) {
+            let top = border()
+            addSubview(top)
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[top(==thickness)]",
+                    options: [],
+                    metrics: ["thickness": thickness],
+                    views: ["top": top]))
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-(0)-[top]-(0)-|",
+                    options: [],
+                    metrics: nil,
+                    views: ["top": top]))
+            borders.append(top)
+        }
+        
+        if edges.contains(.left) || edges.contains(.all) {
+            let left = border()
+            addSubview(left)
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-(0)-[left(==thickness)]",
+                    options: [],
+                    metrics: ["thickness": thickness],
+                    views: ["left": left]))
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[left]-(0)-|",
+                    options: [],
+                    metrics: nil,
+                    views: ["left": left]))
+            borders.append(left)
+        }
+        
+        if edges.contains(.right) || edges.contains(.all) {
+            let right = border()
+            addSubview(right)
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "H:[right(==thickness)]-(0)-|",
+                    options: [],
+                    metrics: ["thickness": thickness],
+                    views: ["right": right]))
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[right]-(0)-|",
+                    options: [],
+                    metrics: nil,
+                    views: ["right": right]))
+            borders.append(right)
+        }
+        
+        if edges.contains(.bottom) || edges.contains(.all) {
+            let bottom = border()
+            addSubview(bottom)
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "V:[bottom(==thickness)]-(0)-|",
+                    options: [],
+                    metrics: ["thickness": thickness],
+                    views: ["bottom": bottom]))
+            addConstraints(
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-(0)-[bottom]-(0)-|",
+                    options: [],
+                    metrics: nil,
+                    views: ["bottom": bottom]))
+            borders.append(bottom)
+        }
+    }
+    
+}
