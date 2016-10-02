0617c0a3f67cab71d6fde380c7705c203b08da8d
Vincent Moore <devdebonair@gmail.com>
2016-10-02 12:53:53 -0400

    converted listing model to all optionals

--- Hamlet/Listing.swift
+++ Hamlet/Listing.swift
@@ -18,47 +18,49 @@ struct Listing: Mappable {
         case controversial = "controversial"
     }
     
-    let author: String
+    let author: String?
     let authorFlair: String?
-    let createdDate: Int
-    let downs: Int
-    let ups: Int
-    let title: String
-    let url: String
+    let createdDate: Double?
+    let downs: Int?
+    let ups: Int?
+    let title: String?
+    let url: URL?
     let isSticky: Bool?
-    let score: Int
-    let isSaved: Bool
-    let isNSFW: Bool
-    let numberOfComments: Int
-    let flair: String
-    let thumbnail: String
-    let domain: String
-    let description: String
-    let name: String
-    let isVisited: Bool
-    let kind: String
+    let score: Int?
+    let isSaved: Bool?
+    let isNSFW: Bool?
+    let numberOfComments: Int?
+    let flair: String?
+    let thumbnail: String?
+    let domain: String?
+    let description: String?
+    let name: String?
+    let isVisited: Bool?
+    let kind: String?
     let previewImage: Preview?
+    let subreddit: String?
     
     init(map: Mapper) throws {
-        try kind = map.from("kind")
-        try author = map.from("data.author")
+        kind = map.optionalFrom("kind")
+        author = map.optionalFrom("data.author")
         authorFlair = map.optionalFrom("data.author_flair_text")
-        try createdDate = map.from("data.created_utc")
-        try downs = map.from("data.downs")
-        try ups = map.from("data.ups")
-        try title = map.from("data.title")
-        try url = map.from("data.url")
-        try isSticky = map.from("data.stickied")
-        try score = map.from("data.score")
-        try isSaved = map.from("data.saved")
-        try isNSFW = map.from("data.over_18")
-        try numberOfComments = map.from("data.num_comments")
-        try flair = map.from("data.link_flair_text")
-        try thumbnail = map.from("data.thumbnail")
-        try domain = map.from("data.domain")
-        try description = map.from("data.selftext")
-        try name = map.from("data.name")
-        try isVisited = map.from("data.visited")
+        createdDate = map.optionalFrom("data.created_utc")
+        downs = map.optionalFrom("data.downs")
+        ups = map.optionalFrom("data.ups")
+        title = map.optionalFrom("data.title")
+        url = map.optionalFrom("data.url")
+        isSticky = map.optionalFrom("data.stickied")
+        score = map.optionalFrom("data.score")
+        isSaved = map.optionalFrom("data.saved")
+        isNSFW = map.optionalFrom("data.over_18")
+        numberOfComments = map.optionalFrom("data.num_comments")
+        flair = map.optionalFrom("data.link_flair_text")
+        thumbnail = map.optionalFrom("data.thumbnail")
+        domain = map.optionalFrom("data.domain")
+        description = map.optionalFrom("data.selftext")
+        name = map.optionalFrom("data.name")
+        isVisited = map.optionalFrom("data.visited")
         previewImage = map.optionalFrom("data.preview")
+        subreddit = map.optionalFrom("data.subreddit")
     }
 }
