4cb757ee652a9a349949da7d282a34bf639d615e
Vincent Moore <devdebonair@gmail.com>
2016-10-02 17:29:10 -0400

    made gfycat variable members optional

--- Hamlet/Gfycat.swift
+++ Hamlet/Gfycat.swift
@@ -10,36 +10,42 @@ import Mapper
 import Alamofire
 
 struct Gfycat: Mappable {
-    let id: String
-    let name: String
-    let number: String
-    let username: String
-    let views: Int
-    let title: String
-    let tags: [String]
-    private let _width: String
-    private let _height: String
+    let id: String?
+    let name: String?
+    let number: String?
+    let username: String?
+    let views: Int?
+    let title: String?
+    let tags: [String]?
+    private let _width: String?
+    private let _height: String?
     let urlMP4: URL?
     let urlGIF: URL?
     
     var width: Int? {
+        guard let _width = _width else {
+            return 0
+        }
         return Int(_width)
     }
     
     var height: Int? {
+        guard let _height = _height else {
+            return 0
+        }
         return Int(_height)
     }
     
     init(map: Mapper) throws {
-        try id = map.from("gfyId")
-        try name = map.from("gfyName")
-        try number = map.from("gfyNumber")
-        try username = map.from("userName")
-        try views = map.from("views")
-        try title = map.from("title")
-        try tags = map.from("tags")
-        try _width = map.from("width")
-        try _height = map.from("height")
+        id = map.optionalFrom("gfyId")
+        name = map.optionalFrom("gfyName")
+        number = map.optionalFrom("gfyNumber")
+        username = map.optionalFrom("userName")
+        views = map.optionalFrom("views")
+        title = map.optionalFrom("title")
+        tags = map.optionalFrom("tags")
+        _width = map.optionalFrom("width")
+        _height = map.optionalFrom("height")
         urlMP4 = map.optionalFrom("mp4Url")
         urlGIF = map.optionalFrom("gifUrl")
     }
@@ -51,6 +57,7 @@ extension Gfycat {
             guard let json = response.result.value as? NSDictionary, let gfyData = json["gfyItem"] as? NSDictionary else {
                 return completion(nil)
             }
+            print(gfyData)
             let gfycat = Gfycat.from(gfyData)
             completion(gfycat)
         }
