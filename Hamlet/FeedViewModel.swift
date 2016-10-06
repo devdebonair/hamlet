c2e985201f1f9f60f575b5b0801375acff278719
Vincent Moore <devdebonair@gmail.com>
2016-10-06 02:41:05 -0400

    added view model to represent data inside feed view controller

---
+++ Hamlet/FeedViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewModel.swift
+//  Hamlet
+//
+//  Created by Vincent Moore on 10/5/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct FeedViewModel {
+    enum MediaType: Int {
+        case photo = 0
+        case video = 1
+        case none = 2
+    }
+    let title: String
+    let description: String
+    let flashMessage: String?
+    let flashColor: UIColor?
+    let author: String
+    let subreddit: String
+    let domain: String
+    let media: Media?
+    let actionColor: UIColor
+    let submission: String
+    let linkUrl: URL?
+}
