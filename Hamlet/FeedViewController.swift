6e35d2edda153b01b448a03389dfe982988f86e4
Vincent Moore <devdebonair@gmail.com>
2016-10-06 02:40:37 -0400

    removed business logic from feed view controller to home presenter

--- Hamlet/FeedViewController.swift
+++ Hamlet/FeedViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 import SnapKit
 import Kingfisher
 
+protocol FeedControllerDelegate {
+    func dataSource() -> [FeedViewModel]
+    func didLoad()
+    func didReachEnd()
+}
+
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     enum CellType: Int {
@@ -40,7 +46,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return tv
     }()
     
-    var listings = [Listing]()
+    var delegate: FeedControllerDelegate!
+    var dataSource: [FeedViewModel] {
+        return delegate.dataSource()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,18 +63,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             make.top.equalTo(view).offset(20)
         }
         
-        Subreddit.fetchHotListing(subreddit: "animegifs") { (listings) in
-            self.listings = listings
-            self.tableView.reloadData()
-        }
+        delegate.didLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return listings.count
+        return dataSource.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,18 +79,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let listing = listings[section]
-        
+        let feedItem = dataSource[section]
         let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 14)
-        label.text = listing.title
+        label.text = feedItem.title
         label.numberOfLines = 0
         label.textColor = .darkText
         
         let container = UIView()
         container.backgroundColor = UIColor.white.withAlphaComponent(0.99)
         
-        if listing.previewMedia != nil {
+        if let _ = feedItem.media {
             container.addBorder(edges: .bottom, colour: UIColor.lightGray.withAlphaComponent(0.5), thickness: 0.4)
         }
         
@@ -98,14 +103,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let listing = listings[indexPath.section]
+        let feedItem = dataSource[indexPath.section]
         var identifier: String
         
         switch indexPath.row {
         case CellType.media.rawValue:
-            identifier = listing.isVideo ? AsyncVideoTableViewCell.IDENTIFIER : PhotoTableViewCell.IDENTIFIER
+            identifier = feedItem.media?.type == .video ? AsyncVideoTableViewCell.IDENTIFIER : PhotoTableViewCell.IDENTIFIER
         case CellType.flash.rawValue:
-            identifier = listing.isAlbum ? FlashTableViewCell.IDENTIFIER : BlankTableViewCell.IDENTIFIER
+            identifier = feedItem.flashMessage == nil ? BlankTableViewCell.IDENTIFIER : FlashTableViewCell.IDENTIFIER
         case CellType.action.rawValue:
             identifier = ActionTableViewCell.IDENTIFIER
         case CellType.description.rawValue:
@@ -123,50 +128,37 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.preservesSuperviewLayoutMargins = false
         cell.selectionStyle = .none
         
-        if let cell = cell as? PhotoTableViewCell, let preview = listing.previewMedia, indexPath.row == CellType.media.rawValue {
-            if listing.domain == Listing.Domain.imgur.rawValue {
-                cell.imagePhoto.kf.setImage(with: listing.url)
-            } else {
-                cell.imagePhoto.kf.setImage(with: preview.variantSource.source.url)
-            }
-            let mediaSize = CGSize(width: preview.variantSource.source.width, height: preview.variantSource.source.height)
+        if let cell = cell as? PhotoTableViewCell, let media = feedItem.media, indexPath.row == CellType.media.rawValue {
+            let mediaSize = CGSize(width: media.width, height: media.height)
             let height = aspectHeight(tableView.frame.size, mediaSize)
+            cell.imagePhoto.kf.setImage(with: media.url)
             cell.setPhotoHeight(height)
         }
         
-        if let cell = cell as? AsyncVideoTableViewCell, let preview = listing.previewMedia, listing.isVideo {
-            let mediaSize = CGSize(width: preview.variantSource.source.width, height: preview.variantSource.source.height)
-            let height = aspectHeight(tableView.frame.size, mediaSize)
-            cell.setMediaHeight(height: height)
+        if let cell = cell as? AsyncVideoTableViewCell, let media = feedItem.media, media.type == .video {
+            if feedItem.domain.lowercased() != Listing.Domain.gfycat.rawValue.lowercased() {
+                let mediaSize = CGSize(width: media.width, height: media.height)
+                let height = aspectHeight(tableView.frame.size, mediaSize)
+                cell.setMediaHeight(height: height)
+                cell.setVideoUrl(url: media.url)
+            }
             
-            switch listing.domain {
-            case Listing.Domain.imgur.rawValue:
-                cell.setVideoUrl(url: Imgur.replaceGIFV(url: listing.url))
-            case Listing.Domain.reddit.rawValue:
-                cell.setVideoUrl(url: preview.variantMP4?.source.url)
-            case Listing.Domain.gfycat.rawValue:
-                Gfycat.fetch(url: listing.url, completion: { (gfycat) in
+            if feedItem.domain.lowercased().contains(Listing.Domain.gfycat.rawValue.lowercased()) {
+                Gfycat.fetch(url: media.url, completion: { (gfycat) in
                     if let gfycat = gfycat {
                         cell.setVideoUrl(url: gfycat.urlMP4)
                     }
                 })
-            default:
-                if let url = preview.variantMP4?.source.url {
-                    cell.setVideoUrl(url: url)
-                } else {
-                    cell.setVideoUrl(url: nil)
-                }
             }
         }
         
-        if let cell = cell as? FlashTableViewCell {
-            let colorOfProgress = UIColor(red: 25/255, green: 181/255, blue: 254/255, alpha: 1.0)
-            cell.colorProgress = colorOfProgress
+        if let cell = cell as? FlashTableViewCell, let message = feedItem.flashMessage, let color = feedItem.flashColor {
+            cell.colorProgress = color
             cell.labelMessage.textColor = .white
             cell.labelMessage.font = UIFont.systemFont(ofSize: 12, weight: UIFontWeightBold)
-            cell.message = "album".uppercased()
+            cell.message = message.uppercased()
             cell.setProgression(progress: 1.0)
-            cell.contentView.superview?.backgroundColor = colorOfProgress
+            cell.contentView.superview?.backgroundColor = color
             let imageView = UIImageView(image: #imageLiteral(resourceName: "disclose"))
             imageView.frame.size = CGSize(width: 13, height: 13)
             imageView.contentMode = .scaleAspectFit
@@ -176,20 +168,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         if let cell = cell as? ActionTableViewCell {
             cell.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
-            cell.tintColor = UIColor(red: 165/255, green: 165/255, blue: 165/255, alpha: 1.0)
+            cell.tintColor = feedItem.actionColor
         }
         
         if let cell = cell as? LabelTableViewCell, indexPath.row == CellType.description.rawValue {
-            cell.labelContent.text = listing.descriptionEscaped
+            cell.labelContent.text = feedItem.description
         }
         
         if let cell = cell as? LabelTableViewCell, indexPath.row == CellType.submission.rawValue {
-            let rgbValue: CGFloat = 164/255
-            let domainSubmissionText = listing.domainExcludeSubreddit.isEmpty ? "" : "\n\n\(listing.domainExcludeSubreddit.uppercased())"
-            let submissionText = "Submitted 1 hour ago by \(listing.author) on r/\(listing.subreddit)\(domainSubmissionText)"
-            cell.labelContent.textColor = UIColor(red: rgbValue, green: rgbValue, blue: rgbValue, alpha: 1.0)
+            cell.labelContent.textColor = UIColor(red: 164/255, green: 164/255, blue: 164/255, alpha: 1.0)
             cell.labelContent.font = UIFont.systemFont(ofSize: 12.0)
-            cell.labelContent.attributedText = NSAttributedString(string: submissionText)
+            cell.labelContent.attributedText = NSAttributedString(string: feedItem.submission)
         }
         
         if let cell = cell as? BlankTableViewCell {
@@ -198,6 +187,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             } else {
                 cell.setHeight(height: 0)
             }
+        }
+        
+        if indexPath.section == dataSource.count - 1 && indexPath.row == 0 {
+            delegate.didReachEnd()
         }
         
         return cell
