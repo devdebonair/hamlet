2f99b8a1e617611ad2db3a8c4c89e782cb3f2e1b
Vincent Moore <devdebonair@gmail.com>
2016-10-22 03:57:25 -0400

    added view model for subreddit list view controller

---
+++ Hamlet/SubredditListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SubredditListViewModel.swift
+//  Hamlet
+//
+//  Created by Vincent Moore on 10/22/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+
+struct SubredditListViewModel {
+    let name: String
+    let imageURL: URL?
+}
