f523cc20b0b4bc556e7dc6eb578bd88298abe0f8
Vincent Moore <devdebonair@gmail.com>
2016-10-26 15:08:13 -0400

    FeedViewController: removed avasset cache management; using tableNode version of willDisplayNode

--- Hamlet/FeedViewController.swift
+++ Hamlet/FeedViewController.swift
@@ -38,7 +38,6 @@ class FeedViewController: ASViewController<ASTableNode>, ASTableDelegate, ASTabl
     
     var delegate: FeedControllerDelegate!
     var dataSource: [FeedViewModel] { return delegate.dataSource() }
-    var assetsCache = [Int:AVAsset]()
     
     init() {
         super.init(node: ASTableNode(style: .plain))
@@ -232,22 +231,15 @@ class FeedViewController: ASViewController<ASTableNode>, ASTableDelegate, ASTabl
             }
         }
     }
-
-    func tableView(_ tableView: ASTableView, willDisplayNodeForRowAt indexPath: IndexPath) {
-        let feedItem = dataSource[indexPath.section]
-        let cell = tableView.nodeForRow(at: indexPath)
-        let weakSelf = self
-        if let cell = cell as? CellNodeVideo, cell.videoPlayer.asset == nil {
-            if let asset = assetsCache[indexPath.section] {
-                cell.videoPlayer.asset = asset
-            } else {
-                DispatchQueue.global(qos: .background).async {
-                    if let media = feedItem.media, let url = media.url {
-                        let asset = AVAsset(url: url)
-                        weakSelf.assetsCache[indexPath.section] = asset
-                        DispatchQueue.main.async {
-                            cell.videoPlayer.asset = asset
-                        }
+    
+    func tableNode(_ tableNode: ASTableNode, willDisplayRowWith node: ASCellNode) {
+        if let node = node as? CellNodeVideo, let indexPath = tableNode.indexPath(for: node), node.videoPlayer.asset == nil {
+            let feedItem = dataSource[indexPath.section]
+            DispatchQueue.global(qos: .background).async {
+                if let media = feedItem.media, let url = media.url {
+                    let asset = AVAsset(url: url)
+                    DispatchQueue.main.async {
+                        node.videoPlayer.asset = asset
                     }
                 }
             }
