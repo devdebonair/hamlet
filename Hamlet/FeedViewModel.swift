9a63c178f9fe0d9c8691984454bff11d21c3dd99
Vincent Moore <devdebonair@gmail.com>
2016-11-02 02:30:32 -0400

    coupled FeedViewModel to cell type creation

--- Hamlet/FeedViewModel.swift
+++ Hamlet/FeedViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import AsyncDisplayKit
 import UIKit
 
 struct FeedViewModel {
@@ -28,4 +29,48 @@ struct FeedViewModel {
     let linkUrl: URL?
     let primaryKey: String
     let upvotes: Int
+}
+
+extension FeedViewModel {
+    
+    enum CellType: Int {
+        case title = 0
+        case photo = 1
+        case flash = 2
+        case action = 3
+        case upvotes = 4
+        case description = 5
+        case submission = 6
+        case blank = 7
+        case separator = 8
+        case mediaDescription = 9
+        case video = 10
+    }
+    
+    func getNodeTypeOrder() -> [CellType] {
+        var types = [CellType]()
+        types.append(.title)
+        
+        if let media = self.media {
+            switch media.type {
+            case .photo:
+                types.append(.photo)
+            case .video:
+                types.append(.video)
+            }
+        } else if self.description.characters.count > 0 {
+            types.append(.mediaDescription)
+        }
+        
+        if self.flashColor != nil || self.flashMessage != nil { types.append(.flash) }
+        types.append(.action)
+        types.append(.separator)
+        if self.upvotes > 0 { types.append(.upvotes) }
+        
+        if self.description.characters.count > 0 && !types.contains(.mediaDescription) { types.append(.description) }
+        
+        if !self.submission.isEmpty { types.append(.submission) }
+        types.append(.blank)
+        return types
+    }
 }
