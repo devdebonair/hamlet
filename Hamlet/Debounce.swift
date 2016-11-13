1a69fd5632a538c2270d4b92bc9ce9b02eba3ad0
Vincent Moore <devdebonair@gmail.com>
2016-11-13 17:04:03 -0500

    added Debounce class

---
+++ Hamlet/Debounce.swift
@@ -0,0 +1,43 @@
+//
+//  Debounce.swift
+//  Hamlet
+//
+//  Created by Vincent Moore on 11/13/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+
+class Debouncer {
+    
+    // Callback to be debounced
+    // Perform the work you would like to be debounced in this callback.
+    var callback: (() -> Void)?
+    
+    private let interval: TimeInterval // Time interval of the debounce window
+    
+    init(interval: TimeInterval) {
+        self.interval = interval
+    }
+    
+    private var timer: Timer?
+    
+    // Indicate that the callback should be called. Begins the debounce window.
+    func call() {
+        // Invalidate existing timer if there is one
+        timer?.invalidate()
+        // Begin a new timer from now
+        timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(handleTimer), userInfo: nil, repeats: false)
+    }
+    
+    @objc private func handleTimer(_ timer: Timer) {
+        if callback == nil {
+            NSLog("Debouncer timer fired, but callback was nil")
+        } else {
+            NSLog("Debouncer timer fired")
+        }
+        callback?()
+        callback = nil
+    }
+    
+}
